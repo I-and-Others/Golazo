d8e974a50118b95c4fa8c80e026026e5ebbdea50
mvollstagg <mvollstagg@gmail.com>
2022-06-12 18:29:53 +0300

    Player cam controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,89 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private Vector2 startTouchPosition;
+    private Vector2 currentPosition;
+    private Vector2 endTouchPosition;
+    private bool stopTouch = false;
+    private bool isGrounded = false;
+    
+    public float swipeRange;
+    public float tapRange;
+    public float playerSpeed;
     void Update()
     {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
+        Swipe();
+    }
 
-            if(touch.deltaPosition.x > 50.0f)
-                gameObject.transform.position = new Vector3(transform.position.x + 0.25f, transform.position.y, transform.position.z);
-            if(touch.deltaPosition.x < -50.0f)
-                gameObject.transform.position = new Vector3(transform.position.x - 0.25f, transform.position.y, transform.position.z);
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Road")
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision other) {
+        if(other.gameObject.tag == "Road")
+            isGrounded = false;
+    }
+
+    public void Swipe()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPosition = Input.GetTouch(0).position;
         }
 
-        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3( 0, 0, 1), ForceMode.Acceleration);
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            currentPosition = Input.GetTouch(0).position;
+            Vector2 Distance = currentPosition - startTouchPosition;
+
+            if (!stopTouch)
+            {
+
+                if (Distance.x < -swipeRange)
+                {
+                    gameObject.transform.position = new Vector3(transform.position.x - 0.25f, transform.position.y, transform.position.z);
+                    stopTouch = true;
+                }
+                else if (Distance.x > swipeRange)
+                {
+                    gameObject.transform.position = new Vector3(transform.position.x + 0.25f, transform.position.y, transform.position.z);
+                    stopTouch = true;
+                }
+                else if (Distance.y > swipeRange)
+                {
+                    if(isGrounded)
+                    {
+                        gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 222f);
+                    }
+                    stopTouch = true;
+                }
+                // else if (Distance.y < -swipeRange)
+                // {
+                //     outputText.text = "Down";
+                //     stopTouch = true;
+                // }
+
+            }
+
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            stopTouch = false;
+
+            endTouchPosition = Input.GetTouch(0).position;
+
+            Vector2 Distance = endTouchPosition - startTouchPosition;
+
+            if (Mathf.Abs(Distance.x) < tapRange && Mathf.Abs(Distance.y) < tapRange)
+            {
+                // outputText.text = "Tap";
+            }
+
+        }
+
+        gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * Time.deltaTime * playerSpeed);
+        // gameObject.transform.Translate(Vector3.forward * Time.deltaTime * 3f, Space.World);
     }
 }
